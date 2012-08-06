bafa2eaff89cfd7832944beb454d8b9b389ef42d
Pierre-Yves Chibon <pingou@pingoured.fr>
2012-08-06 13:10:45 +0200

    Finish writing the parser in D

--- D/read_fasta.d
+++ D/read_fasta.d
@@ -6,18 +6,85 @@
  */
 
 import std.stdio;
+import std.string;
+import std.algorithm;
+
+class Sequence {
+    private:
+        string _name;
+        string _sequence;
+
+    public:
+        ~this() { }
+        string getName()
+        {
+            return _name;
+        }
+
+        void setName(string name)
+        {
+            _name = name;
+        }
+
+        string getSequence()
+        {
+            return _sequence;
+        }
+
+        void setSequence(string sequence)
+        {
+            _sequence = sequence;
+        }
+
+        void appendSequence(string sequence)
+        {
+            _sequence = _sequence ~ sequence;
+        }
+
+        ulong getSequenceLength()
+        {
+            return _sequence.length;
+        }
+}
 
 int main()
 {
-    writefln("%s World!", "Hello");
-    
-    File file = File("../fasta.sample", "r");
+    File file = File("../sample.fasta", "r");
 
+    Sequence[] sequences;
+    Sequence sequence;
+    string header_start = ">";
     while (!file.eof())
     {
-        string line = chomp(file.readln());
-        writeln("read line -> |", line);
+        string line = file.readln().chomp;
+        if (line && strip(line))
+        {
+            if (line.startsWith(">"))
+            {
+                if (sequence)
+                {
+                    ++sequences.length;
+                    sequences[sequences.length - 1] = sequence;
+                }
+                sequence = new Sequence();
+                sequence.setName(line[1..line.length]);
+            } else {
+                sequence.appendSequence(line);
+            }
+        }
+    }
+    if (sequence)
+    {
+        ++sequences.length;
+        sequences[sequences.length - 1] = sequence;
     }
     file.close();
+
+    for (int cnt; cnt < sequences.length; cnt++)
+    {
+        writefln("Sequence: %s -- %d", sequences[cnt].getName(),
+            sequences[cnt].getSequenceLength());
+    }
+
     return 0;
 }
